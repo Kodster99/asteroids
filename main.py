9317ce43443dd09d6d640cb8926a0e785ee92b46
Kodster99 <kody25@gmail.com>
2024-11-25 17:04:36 +0800

    Adding player rotation

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-
+    
     while True:
+        player.update(dt)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
